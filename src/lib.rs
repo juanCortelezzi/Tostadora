0c05577f8443e3bc84ae4216674626f7c0f6bb41
juancortelezzi <juanbicortelezzi@gmail.com>
2021-11-11 00:59:49 -0300

    added lib utils and commands

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+pub mod docker;
+pub mod light;
+pub mod pipewire;
+pub mod sound;
+
+pub mod commands {
+    use std::ffi::OsStr;
+    use std::process::Command;
+
+    pub fn run_cmd_notify<I, S>(cmd: &str, args: I, signal: &str)
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        run_cmd(cmd, args);
+        run_cmd(
+            "pkill",
+            [format!("-RTMIN+{}", signal), "dwmblocks".to_owned()],
+        );
+    }
+
+    pub fn run_cmd<I, S>(cmd: &str, args: I)
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        Command::new(cmd)
+            .args(args)
+            .status()
+            .expect("Could not execute command")
+            .success();
+    }
+}
+
+pub mod test {
+    use clap::ArgMatches;
+    pub fn handle(args: &ArgMatches) {
+        println!("{}", args.value_of("ECHO").expect("No ECHO value provided"))
+    }
+}
